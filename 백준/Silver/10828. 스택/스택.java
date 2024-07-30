1dedba005e311f2424f8c35b67a3b0be3b3e6272
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-07-30 22:39:09 +0900

    [Silver IV] Title: 스택, Time: 364 ms, Memory: 18116 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -5,46 +5,68 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int[] stack = new int[10001]; // 명령이 10000개까지라 길이가10000인 배열
+	static int size;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int T = Integer.parseInt(br.readLine()); // 테스트케이스
-		Stack<Integer> stack = new Stack<>(); // 스택
+		
 		
 		for (int tc=0; tc<T;tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			switch(st.nextToken()) {
 			case "push":
-				stack.push(Integer.parseInt(st.nextToken()));
+				push(Integer.parseInt(st.nextToken()));
 				break;
 			case "pop":
-				if (stack.empty()) {
-					System.out.println(-1);
-				}
-				else{
-					System.out.println(stack.pop());
-				}
+				System.out.println(pop());
 				break;
 			case "size":
-				System.out.println(stack.size());
+				System.out.println(size());
 				break;
 			case "empty":
-				if(stack.empty()) {
-					System.out.println(1);
-				}
-				else {
-					System.out.println(0);
-				}
+				System.out.println(empty());
 				break;
 			case "top":
-				if(stack.empty()) {
-					System.out.println(-1);
-				}
-				else {
-					System.out.println(stack.peek());
-				}
+				System.out.println(top());
 				break;
 			}
 		}
-	}	
+	}
+	public static void push(int num) {
+		stack[size] = num;
+		size++;
+	}
+	public static int pop() {
+		if (size==0) {
+			return -1;
+		}
+		else {
+			int P = stack[size-1];
+			size--;
+			return P;
+		}
+	}
+	public static int size() {
+		return size;
+	}
+	public static int empty() {
+		if (size==0) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	public static int top() {
+		if (size==0) {
+			return -1;
+		}
+		else {
+			int T = stack[size-1];
+			return T;
+		}
+	}
+	
 }
