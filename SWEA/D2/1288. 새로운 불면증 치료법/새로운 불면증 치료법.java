fdd6da9612f2b390b00dc0bff1dbe18505cf2f1b
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-08-16 16:25:13 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 113 ms, Memory: 18,352 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -4,31 +4,34 @@ import java.io.InputStreamReader;
 
 public class Solution {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
-        
-        for (int tc = 1; tc <= T; tc++) {
-            int num = Integer.parseInt(br.readLine());
-            int first = num;
-            boolean[] digits = new boolean[10];  // 0~9까지 숫자가 등장했는지 확인하는 배열
-            int count = 0; // 등장한 숫자 개수
-            
-            while (true) {
-                String str = String.valueOf(num);
-                for (int i = 0; i < str.length(); i++) {
-                    int digit = str.charAt(i) - '0';
-                    if (!digits[digit]) {  // 처음 등장한 숫자라면
-                        digits[digit] = true;
-                        count++;
-                    }
-                }
-                if (count == 10) break;  // 모든 숫자가 등장했다면 루프 종료
-                
-                num += first;
-            }
-            System.out.println("#" + tc + " " + num);
-        }
-    }
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int num = Integer.parseInt(br.readLine());
+			int first = num;
+			int[] arr = new int[10];
+			for(int i=0;i<=9;i++) {
+				arr[i]=1;
+			}
+			while(true) {
+				String str = String.valueOf(num);
+				for(int i=0;i<str.length();i++) {
+					arr[str.charAt(i)-'0']=0;     
+				}
+				
+				int sum = 0;
+				for(int a=0;a<arr.length;a++) {
+					sum+=arr[a];
+				}
+				
+				if (sum==0) break;
+				num = num+first;
+			}
+			System.out.println("#"+tc+" "+num);
+		}
+	}
 }
+
