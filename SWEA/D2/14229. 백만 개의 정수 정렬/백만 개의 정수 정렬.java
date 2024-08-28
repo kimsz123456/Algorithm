7fd1a6fa1605aa8ce703f991baecdd718e4f7061
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-08-28 14:18:49 +0900

    [D2] Title: 백만 개의 정수 정렬, Time: 476 ms, Memory: 111,412 KB -BaekjoonHub

--- SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
+++ SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
@@ -4,60 +4,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] tmp = new int[1000000];
-	static int count = 0;
-	static int result = -1;
+	static int[] arr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
 		int N = 1000000; // 배열의 크기
-		int[] A = new int[N];
+		arr = new int[N];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		merge_sort(A, 0, N - 1);
-		System.out.println(A[500000]);
+		quickSort(arr, 0, N - 1);
+		System.out.println(arr[500000]);
 	}
 
-	static void merge_sort(int[] arr, int p, int r) {
-		if (p < r) {
-			int q = (p + r) / 2;
-			merge_sort(arr, p, q);
-			merge_sort(arr, q + 1, r);
-			merge(arr, p, q, r);
+	static void quickSort(int[] arr, int left, int right) {
+		if (left < right) {
+			int pivot = partition(arr, left, right);
+			quickSort(arr,left, pivot - 1);
+			quickSort(arr,pivot + 1, right);
 		}
 	}
 
-	static void merge(int[] arr, int p, int q, int r) {
-		int i = p;
-		int j = q + 1;
-		int t = 0;
-		while (i <= q && j <= r) {
-			if (arr[i] <= arr[j]) {
-				tmp[t++] = arr[i++];
-			} else {
-				tmp[t++] = arr[j++];
+	private static int partition(int[] arr, int left, int right) {
+		int pivot = arr[right];
+		int i = left-1;
+		
+		for(int j = left; j<right; j++) {
+			if(arr[j] <= pivot) {
+				i++;
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
 			}
-			count++;
 		}
-		while (i <= q) {
-			tmp[t++] = arr[i++];
-			count++;
-		}
-		while (j <= r) {
-			tmp[t++] = arr[j++];
-			count++;
-		}
-		// 병합된 결과를 원래 배열에 복사
-		i = p;
-		t = 0;
-		while (i <= r) {
-			arr[i++] = tmp[t++];
-		}
+		int tmp = arr[i+1];
+		arr[i+1] = arr[right];
+		arr[right] = tmp;
+		return i+1;
 	}
 }
