e56fac5575a983d69333e347a404377fbc21a105
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-08-29 16:53:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 124 ms, Memory: 23,356 KB -BaekjoonHub

---
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int result;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringBuilder sb = new StringBuilder();
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			result = 0;
+			int[][] field = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					field[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int[] arr1 = new int[N];
+			int[] arr2 = new int[N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					arr1[j] = field[i][j];
+					arr2[j] = field[j][i];
+				}
+				airstrip(arr1, X);
+				airstrip(arr2, X);
+			}
+			sb.append("#").append(tc).append(" ").append(result);
+			System.out.println(sb.toString());
+		}
+	}
+
+	static void airstrip(int[] arr, int X) {
+		// 경사로 설치
+		boolean[] lift = new boolean[arr.length];
+		// 정방향
+		for (int i = 0; i < arr.length - 1; i++) {
+			// 올라갈때
+			if (arr[i] < arr[i + 1] && arr[i] + 1 == arr[i + 1]) {
+				// 경사로 설치해보기
+				for (int j = X; j > 0; j--) {
+					// 범위도 맞고, 평지일때
+					if (i + 1 - j >= 0 && arr[i + 1 - j] == arr[i + 1] - 1 && !lift[i + 1 - j]) {
+						lift[i + 1 - j] = true;
+					}
+					// 안맞으면
+					else {
+						return;
+					}
+				}
+			}
+			// 내려갈때
+			else if (arr[i] > arr[i + 1] && arr[i] - 1 == arr[i + 1]) {
+				// 경사로 설치해보기
+				for (int j = 0; j < X; j++) {
+					// 범위도 맞고, 평지일때
+					if (i + 1 + j < arr.length && arr[i + 1 + j] == arr[i] - 1 && !lift[i + 1 + j]) {
+						lift[i + 1 + j] = true;
+					}
+					// 안맞으면
+					else {
+						return;
+					}
+				}
+			}
+			// 평지
+			else if (arr[i] == arr[i + 1]) {
+				continue;
+			}
+			// 그 외
+			else {
+				return;
+			}
+		}
+		result++;
+	}
+}
