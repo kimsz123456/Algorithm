582f775bc28c5676924e3af13b96e0f9df2e2127
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-09-01 16:44:02 +0900

    [Silver III] Title: N과 M (3), Time: 344 ms, Memory: 112040 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] selected;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		selected = new int[M];
+		select(0);
+		System.out.println(sb);
+	}
+
+	private static void select(int count) {
+		if (count == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			selected[count] = i + 1;
+			select(count+1);
+		}
+	}
+}
