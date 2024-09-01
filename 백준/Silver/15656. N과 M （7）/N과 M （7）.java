5e64eb62540bbdf3e11dc01911fd70f8fa3385d8
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-09-01 17:25:26 +0900

    [Silver III] Title: N과 M (7), Time: 552 ms, Memory: 220176 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static boolean[] visited;
+	static int[] selected, num;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		num = new int[N];
+		visited = new boolean[N];
+		selected = new int[M];
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st2.nextToken());
+		}
+		Arrays.sort(num);
+		select(0, 0);
+		System.out.println(sb);
+	}
+
+	private static void select(int idx, int count) {
+		if (count == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		if (idx >= N) {
+			return;
+		}
+		for(int i=0;i<N;i++) {
+			selected[count] = num[i];
+			select(i, count + 1);
+		}
+		
+	}
+}
