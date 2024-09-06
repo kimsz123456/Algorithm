8a25fd53fbd5788f17c5fceb96c609e8b79df352
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-09-06 11:43:26 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 111 ms, Memory: 18,372 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int N,B,min;
+	static int[] height;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int tc=1;tc<=T;tc++) {
+        	st = new StringTokenizer(br.readLine());
+        	N = Integer.parseInt(st.nextToken());
+        	B = Integer.parseInt(st.nextToken());
+        	
+        	height = new int[N];
+        	st = new StringTokenizer(br.readLine());
+        	for(int i=0;i<N;i++) {
+        		height[i]=Integer.parseInt(st.nextToken());
+        	}
+        	min = Integer.MAX_VALUE;
+        	makeTeam(0,0);
+        	System.out.println("#"+tc+" "+(min-B));
+        }
+        
+        
+    }
+    
+    static void makeTeam(int idx,int sum) {
+    	if(idx==N) {
+    		if(sum>=B) {
+    			min = Math.min(min, sum);
+    		}
+    		return;
+    	}
+    	makeTeam(idx+1,sum);
+    	if(sum<B) {
+    		makeTeam(idx+1,sum+height[idx]);
+    	}
+    }
+}
