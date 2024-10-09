e272037888ff2331ca584b1854da5dc6904d7934
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-10-09 12:53:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 432 ms, Memory: 125248 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -26,30 +26,34 @@ public class Main {
         
         for(int i=1;i<N;i++) {
         	
-        	// 수열의 다음값
-        	int next = arr[i];
+        	// 수열의 현재 값
+        	int now = arr[i];
         	
-        	// lis의 현재값보다 크면 이어붙이기
-        	if(lis[length-1]<next) {
+        	// lis의 끝값보다 크면 이어붙이기
+        	if(lis[length-1]<now) {
         		length++;
-        		lis[length-1]=next;
+        		lis[length-1]=now;
         	}
         	
-        	// lis의 현재값보다 작거나 같으면 이분탐색을 통해 바꿀 곳 찾기
+        	// lis의 끝값보다 작거나 같으면 이분탐색을 통해 바꿀 곳 찾기
         	else {
         		int left = 0;
         		int right = length;
-        		while(left<right) {
+        		int result = 0;
+        		while(left<=right) {
         			int mid = (left+right)/2;
         			
-        			if(lis[mid]<next) {
+        			// lis의 바꿀위치값 < 현재값
+        			if(lis[mid]<now) {
+        				result = Math.max(left, result);
         				left = mid+1;
         			}
+        			// lis의 바꿀위치값 >= 현재값
         			else {
-        				right = mid;
+        				right = mid-1;
         			}
         		}
-        		lis[left]=next;
+        		lis[left]=now;
         	}
         }
         System.out.println(length);
