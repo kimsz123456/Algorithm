c5c8a09bedd3c86b00da65bd26bc4cba7c018f63
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-10-17 17:51:57 +0900

    [Gold IV] Title: N-Queen, Time: 2608 ms, Memory: 12084 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[][] arr;
+	static boolean[] col, leftdiagonal, rightdiagonal;
+	static int result;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		arr = new int[N][N];
+		col = new boolean[N];
+		leftdiagonal = new boolean[2 * N - 1];
+		rightdiagonal = new boolean[2 * N - 1];
+		result = 0;
+		nqueen(0, N);
+		System.out.println(result);
+	}
+	public static void nqueen(int row, int N) {
+		if (row == N) {
+			result++;
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (!col[i] && !leftdiagonal[N - 1 - row + i] && !rightdiagonal[row + i]) {
+				col[i] = true;
+				leftdiagonal[N - 1 - row + i] = true;
+				rightdiagonal[row + i] = true;
+				nqueen(row + 1, N);
+				col[i] = false;
+				leftdiagonal[N - 1 - row + i] = false;
+				rightdiagonal[row + i] = false;
+			}
+		}
+	}
+}
