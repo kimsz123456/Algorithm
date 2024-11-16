9fed2c5a546c53c37e88a1e857af6f0ca12aaf60
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-11-16 12:34:21 +0900

    [Gold V] Title: 용액, Time: 256 ms, Memory: 32072 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+        
+        st= new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        
+        int left = 0;
+        int right = N-1;
+        
+        int resultL=0;
+        int resultR=0;
+        
+        int sum = 0;
+        int summin = Integer.MAX_VALUE;
+        while(left<right) {
+        	sum = arr[left]+arr[right];
+        	if(Math.abs(sum)<summin) {
+        		summin=Math.abs(sum);
+        		resultL=arr[left];
+        		resultR=arr[right];
+        	}
+        	if(sum>0) {
+        		right--;
+        	}
+        	else {
+        		left++;
+        	}
+        }
+        sb.append(resultL).append(" ").append(resultR);
+        System.out.println(sb);
+    }
+}
