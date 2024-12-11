c272fd319c1046f88875508c1f7bb020f951ead9
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2024-12-11 11:18:52 +0900

    [Gold IV] Title: LCS 2, Time: 104 ms, Memory: 16548 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		char[] str1 = br.readLine().toCharArray();
+		char[] str2 = br.readLine().toCharArray();
+		
+		int len1 = str1.length;
+		int len2 = str2.length;
+		
+		int[][] dp = new int[len1 + 1][len2 + 1];
+		
+		for(int i = 1; i <= len1; i++) {
+			for(int j = 1; j <= len2; j++) {
+				if(str1[i - 1] == str2[j - 1]) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;	
+				}
+				else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		sb.append(dp[len1][len2]).append("\n");
+		Stack<Character> stack = new Stack<>();
+		while(len1>0 && len2>0) {
+			
+			if(len1 == 0 || len2 ==0)break;
+			
+			if(dp[len1][len2] == dp[len1-1][len2]) {
+				len1--;
+			}else if(dp[len1][len2] == dp[len1][len2-1]) {
+				len2--;
+			}else {
+				stack.push(str1[len1-1]);
+				len1--;
+				len2--;
+			}
+			
+			
+		}
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop());
+		}
+		System.out.println(sb);
+	}
+ 
+}
