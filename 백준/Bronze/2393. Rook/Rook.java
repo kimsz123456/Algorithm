25c3b9d4f03c9895772a264cbaf268c20a1da503
Jiseung Kim <152263125+kimsz123456@users.noreply.github.com>
2025-01-12 12:12:40 +0900

    [Bronze V] Title: Rook, Time: 68 ms, Memory: 11376 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    	System.out.print(sb);
+    }
+}
