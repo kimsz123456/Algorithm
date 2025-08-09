2d0f7050213c2445a16d9e229356cbfb54bead06
Jiseung Kim <kimsz123456@gmail.com>
2025-08-09 18:20:13 +0900

    [Bronze III] Title: 곱셈, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static long M;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int A = Integer.parseInt(br.readLine());
+        String B = br.readLine();
+        
+        for(int i=2;i>=0;i--) {
+        	sb.append(A*(B.charAt(i)-'0')).append("\n");
+        }
+        sb.append(A*Integer.parseInt(B));
+        System.out.println(sb);
+    }
+}
