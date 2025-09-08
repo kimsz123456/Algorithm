82167cef770a3399dddfda180e556e47214e47ac
Jiseung Kim <kimsz123456@gmail.com>
2025-09-08 16:12:10 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 216 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -2,77 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, K;
-    static int[] conveyor;
-    static boolean[] robots;
-    static int idx;
+	static int N, K, idx;
+	static int[] conveyor;
+	static boolean[] robots;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-        conveyor = new int[2 * N];
-        robots = new boolean[2 * N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 2 * N; i++) {
-            conveyor[i] = Integer.parseInt(st.nextToken());
-        }
+		conveyor = new int[2 * N];
+		robots = new boolean[2 * N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 2 * N; i++) {
+			conveyor[i] = Integer.parseInt(st.nextToken());
+		}
+		idx = 0;
+		process();
+	}
 
-        idx = 0;
-        System.out.println(process());
-    }
+	static void process() {
+		int step = 1;
+		while (true) {
+			rotate();
+			move();
+			up();
+			if (K < 1) {
+				System.out.println(step);
+				return;
+			}
+			step++;
+		}
+	}
 
-    static int process() {
-        int step = 1;
-        while (true) {
-            rotate();
-            move();
-            up();
+	static void rotate() {
+		idx = (idx + (2 * N - 1)) % (2 * N);
+		int exit = (idx + N - 1) % (2 * N);
+		robots[exit]=false;
+	}
 
-            if (countZero() >= K) {
-                return step;
-            }
-            step++;
-        }
-    }
+	static void move() {
 
-    static void rotate() {
-        idx = (idx + (2 * N - 1)) % (2 * N);
-        int exit = (idx + N - 1) % (2 * N);
-        robots[exit] = false;
-    }
-
-    static void move() {
-        int exit = (idx + N - 1) % (2 * N);
+		int exit = (idx + N - 1) % (2 * N);
         for (int i = N - 2; i >= 0; i--) {
             int pos = (idx + i) % (2 * N);
             int next = (pos + 1) % (2 * N);
-
+            
             if (robots[pos] && !robots[next] && conveyor[next] > 0) {
-                robots[pos] = false;
+            	robots[pos] = false;
                 conveyor[next]--;
+                if (conveyor[next] == 0) {
+                    K--;
+                }
                 if (next != exit) {
-                    robots[next] = true;
+                	robots[next] = true;
                 }
             }
         }
-        robots[exit] = false;
-    }
+	}
 
-    static void up() {
-        if (conveyor[idx] > 0 && !robots[idx]) {
-            robots[idx] = true;
-            conveyor[idx]--;
-        }
-    }
-
-    static int countZero() {
-        int cnt = 0;
-        for (int c : conveyor) {
-            if (c == 0) cnt++;
-        }
-        return cnt;
-    }
+	static void up() {
+		if (conveyor[idx] > 0) {
+			robots[idx]=true;
+			conveyor[idx]--;
+			if (conveyor[idx] == 0) {
+				K--;
+			}
+		}
+	}
 }
