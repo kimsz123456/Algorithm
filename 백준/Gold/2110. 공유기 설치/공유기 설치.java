b34cd953f0c19675d7eb4b821ff8d54b1283d7d2
Jiseung Kim <kimsz123456@gmail.com>
2025-11-27 16:06:02 +0900

    [Gold IV] Title: 공유기 설치, Time: 152 ms, Memory: 12968 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,37 +1,41 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        int N = nextInt();
+        int C = nextInt();
+
         int[] router = new int[N];
         for(int i=0;i<N;i++) {
-        	router[i] = Integer.parseInt(br.readLine());
+            router[i] = nextInt();
         }
         Arrays.sort(router);
-        
+
         int left = 1;
         int right = router[N-1]-router[0]+1;
-        int mid, count;
+        int mid,count;
         while(left<=right) {
-        	mid = (left+right)/2;
-        	count = 1;
-        	int now = router[0];
-            for(int i=1;i<N;i++){
-               if(router[i]-now<mid) continue;
-               count++;
-               now = router[i];
+            mid = (left+right)/2;
+            count = 1;
+            int now = router[0];
+            for(int i=1;i<N;i++) {
+                if(router[i]-now<mid) continue;
+                count++;
+                now = router[i];
             }
             if(count>=C) left = mid+1;
             else right = mid-1;
-        	
         }
         System.out.println(right);
+    }
+    static int nextInt() throws IOException {
+        int c;
+        while (!Character.isDigit(c = System.in.read()));
+        int value = c & 15;
+        while (Character.isDigit(c = System.in.read())) {
+            value = value * 10 + (c & 15);
+        }
+        return value;
     }
 }
